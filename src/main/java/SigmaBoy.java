eb3f040d22aa159757d165d0d4883a726f4e833f
JovianJosh <jovianjosh@gmail.com>
2026-01-30 13:33:35 +0800

    Add level 1: echo functionality

--- src/main/java/SigmaBoy.java
+++ src/main/java/SigmaBoy.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
+
 public class SigmaBoy {
     public static void main(String[] args) {
-        String logo = "____________________________________________________________\n"
-                + "Hello! I'm SigmaBoy\n"
-                + "What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________\n";
-        System.out.println(logo);
+        final String LINE = "____________________________________________________________\n";
+        final String HI = LINE + "Hello! I'm SigmaBoy\nWhat can I do for you?\n" + LINE;
+        final String BYE= " Bye. Hope to see you again soon!\n";
+        System.out.println(HI);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            String userinput = scanner.nextLine();
+
+            System.out.println(LINE);
+            if (userinput.equals("bye")) {
+                System.out.println(BYE);
+                System.out.println(LINE);
+                break;
+            } else {
+                System.out.println(userinput);
+                System.out.println(LINE);
+            }
+        }
+        scanner.close();
     }
 }
